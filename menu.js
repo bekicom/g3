9802bd76118f60f36a5b23944b62a95552673952
Oyatillo09 <obidovo647@gmail.com>
2023-04-25 10:03:13 +0300

    vnejsv

---
+++ menu.js
@@ -0,0 +1,34 @@
+const uy = document.querySelector("bola2")
+function uyCha() {
+    alert("siz uychani bosdingiz")
+}
+
+const vilka = document.querySelector("bola3")
+function vilkaCha() {
+    alert("siz vilkani bosdingiz")
+}
+
+const cart = document.querySelector("bola4")
+function cartCha() {
+    alert("siz cartni bosdingiz")
+}
+
+const nimadir = document.querySelector("bola5")
+function markCha() {
+    alert("siz manzilni bosdingiz")
+}
+
+const exit = document.querySelector("bola6")
+function exitCha() {
+    alert("siz exitni bosdingiz")
+}
+
+const about = document.querySelector("e")
+function aboutON() {
+    alert("siz aboutni bosdingiz")
+}
+
+const menu = document.querySelector("w")
+function menuON() {
+    alert("siz menuni bosdingiz")
+}
